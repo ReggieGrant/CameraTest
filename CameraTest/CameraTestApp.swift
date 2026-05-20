36488785ec755c81f13d72b5eabf57ad4e119f1c
ReggieGrant <grantreggie09@gmail.com>
2026-05-19 17:08:57 -0700

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by Reginald Grant on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
